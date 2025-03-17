8d77b684c060a8d279577e6b6139010192619d7b
chanakin <chrischank97@gmail.com>
2025-03-17 18:47:04 +0000

    kanagawa themed

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,5 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
 -- See the kickstart.nvim README for more information
 return {
   { import = 'custom.plugins.harpoon' },
@@ -8,4 +7,5 @@ return {
   { import = 'custom.plugins.jupyter' },
   { import = 'custom.plugins.python-repl' },
   { import = 'custom.plugins.avante' },
+  { import = 'custom.plugins.kanagawa'}
 }
