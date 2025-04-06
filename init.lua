ae01d385a9deeb3c46dbda851d80a70e0761fb13
Christopher Chan <chrischank@protonmail.ch>
2025-04-06 21:11:43 +0100

    fix: jk nonhighlight was making things slow, removed

--- init.lua
+++ init.lua
@@ -188,8 +188,6 @@ vim.opt.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', 'jk', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', 'kj', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
